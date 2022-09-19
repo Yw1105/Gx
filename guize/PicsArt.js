9ae6da7c5469a281f359cb6fd7eb12f87acb4f83
旭 <79443385+Yw1105@users.noreply.github.com>
2022-09-19 19:26:47 +0800

    Update PicsArt.js

--- guize/PicsArt.js
+++ guize/PicsArt.js
@@ -1,8 +1,25 @@
 /*
-PicsArt app unlocks pro
+PicsArt 解锁高级功能
 
-Surge & QX Mitm = api.picsart.c*, api.meiease.c*
-*/
+***************************
+QuantumultX:
+
+[rewrite_local]
+^https:\/\/api\.(picsart|meiease)\.c(n|om)\/users\/show\/me\.json url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/PicsArt.js
+
+[mitm]
+hostname = api.picsart.c*, api.meiease.c*
+
+***************************
+Surge4 or Loon:
+
+[Script]
+http-response https:\/\/api\.(picsart|meiease)\.c(n|om)\/users\/show\/me\.json requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/PicsArt.js
+
+[MITM]
+hostname = api.picsart.c*, api.meiease.c*
+
+**************************/
 
 let obj = JSON.parse($response.body);
 obj.subscription.granted = "true";
