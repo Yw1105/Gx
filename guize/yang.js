413e170ef812b7780d9aeabf44a68e1ca9232c7b
旭 <79443385+Yw1105@users.noreply.github.com>
2022-09-21 01:49:29 +0800

    Update yang.js

--- guize/yang.js
+++ guize/yang.js
@@ -1,6 +1,6 @@
 #羊了个羊次数
 
 [rewrite_local]
-^https:\/\/.+\.easygame2021\.com\/.+ url response-body "daily_count":\d+,"topic_count":\d+, response-body "daily_count":9999999999999999,"topic_count": 9999999999999999,
+^https:\/\/.+\.easygame2021\.com\/.+ url response-body "daily_count":\d+,"topic_count":\d+, response-body "daily_count":9999e+320,"topic_count": 9999e+320,
 
 hostname = *.easygame2021.com
