44e043dcd7e0a7bc8c524e1d84661dea591ff8b2
旭 <79443385+Yw1105@users.noreply.github.com>
2023-11-24 17:30:30 +0800

    Update Ad.js

--- Quantumult X/Ad.js
+++ Quantumult X/Ad.js
@@ -2,10 +2,12 @@
 #2023-11-03
 
   
-hostname = api.gongkaoleida.com
+hostname = api.gongkaoleida.com,api.8eb269a.com
 
 #公考雷达
 ^https:\/\/api\.gongkaoleida\.com\/api\/v2\/ad\/info\? url reject-dict  
-  
+
+#One  
+^https:\/\/api\.8eb269a\.com\/v2\.5\/ad\/space url reject-dict
 
 
